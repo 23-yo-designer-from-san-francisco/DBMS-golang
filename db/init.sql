75f5d8e12232b60113a1e602b48d30fbda55c274
Leonid <belyaevleonid2000@mail.ru>
2022-01-06 23:58:03 +0300

    Try

--- db/init.sql
+++ db/init.sql
@@ -221,13 +221,13 @@ CREATE UNIQUE INDEX uidx_threads_slug ON public.threads USING btree (slug);
 CREATE UNIQUE INDEX uidx_user_id ON public.users USING btree (id);
 CREATE UNIQUE INDEX uidx_users_email ON public.users USING btree (email);
 
-CREATE INDEX idx_post_threadid_created_id ON public.posts USING btree (thread, created, id);
-CREATE INDEX idx_post_threadid_id_parentid ON public.posts USING btree (thread, id, parent);
-CREATE INDEX idx_post_threadid_id_parentnull_id ON public.posts USING btree (thread, id) WHERE (parent IS NULL);
+CREATE INDEX idx_post_threadid_created_id ON public.posts USING btree (thread, created, id, parent);
+-- CREATE INDEX idx_post_threadid_id_parentid ON public.posts USING btree (thread, id, parent);
+-- CREATE INDEX idx_post_threadid_id_parentnull_id ON public.posts USING btree (thread, id) WHERE (parent IS NULL);
 CREATE INDEX idx_post_threadid_path ON public.posts USING btree (thread, path);
 CREATE INDEX idx_posts_id ON public.posts USING hash (id);
 
-CREATE INDEX idx_threads_created ON public.threads (created); -- Can't work
+-- CREATE INDEX idx_threads_created ON public.threads (created); -- Can't work
 CREATE INDEX idx_threads_slug_hash ON public.threads USING hash (slug);
 CREATE INDEX idx_threads_forum_created ON public.threads USING btree (forum, created);
 
