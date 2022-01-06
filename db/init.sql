7a5d490fd3c07eda50a11093d4d28e6a530922e5
Leonid <belyaevleonid2000@mail.ru>
2022-01-07 00:47:10 +0300

    Revert

--- db/init.sql
+++ db/init.sql
@@ -226,7 +226,7 @@ CREATE INDEX idx_post_threadid_created_id ON public.posts USING btree (thread, c
 -- CREATE INDEX idx_posts_id ON public.posts USING hash (id);
 
 -- CREATE INDEX idx_threads_slug_hash ON public.threads USING hash (slug);
--- CREATE INDEX idx_threads_forum_created ON public.threads USING btree (forum, created);
+CREATE INDEX idx_threads_forum_created ON public.threads USING btree (forum, created);
 
 -- CREATE INDEX idx_forums_slug_hash ON public.forums USING hash (slug);
 -- CREATE INDEX idx_forums_users_foreign ON public.forums USING hash ("user");
